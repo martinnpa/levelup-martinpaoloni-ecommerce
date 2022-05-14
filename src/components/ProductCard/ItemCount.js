591bab3614f3b8d2a91c7b0662cc1e57a2a3983a
martinnpa <paolonimartin@gmail.com>
2022-05-14 14:36:48 -0300

    ItemCount completado

--- src/components/ProductCard/ItemCount.js
+++ src/components/ProductCard/ItemCount.js
@@ -1,5 +1,7 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 import styles from "./Button.module.css";
+import { ShoppingCartIcon } from '@heroicons/react/outline';
+import Button from 'components/Common/Button';
 
 const ItemCount = ({stock}) => {
   const [counter, setCounter] = useState(0);
@@ -17,23 +19,31 @@ const ItemCount = ({stock}) => {
   };
   return (
     <>
-    <button
-      className={styles.operation}
-      onClick={() => handleOperationMinus()}
-      disabled={counter === 0}
-    >
-      -
-    </button>
+      <p className="pt-2 mt-2 mb-3 text-center border-t border-secundary">Stock: {stock - counter}</p>
+      <div className="flex content-center justify-between w-1/2 mx-auto">
+        <button
+          className={styles.operation}
+          onClick={() => handleOperationMinus()}
+          disabled={counter === 0}
+        >
+          -
+        </button>
 
-    {counter}
+        {counter}
 
-    <button
-      className={styles.operation}
-      onClick={() => handleOperationSum()}
-      disabled={counter === stock}
-    >
-      +
-    </button>
+        <button
+          className={styles.operation}
+          onClick={() => handleOperationSum()}
+          disabled={counter === stock}
+        >
+          +
+        </button>
+      </div>
+      <div className="mt-3 text-center">
+        <Button disabled={counter===0} onClick={()=> alert('Aca va la función agregar')}>
+          <ShoppingCartIcon className="relative w-5 mr-1" style={{top: "2px"}}/> <span>Agregar</span>
+        </Button>
+      </div>
     </>
   )
 }
--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,26 +1,14 @@
-import React, {useState} from 'react';
-import Button from 'components/Common/Button';
+import React from 'react';
 import ItemCount from 'components/ProductCard/ItemCount';
-import { ShoppingCartIcon } from '@heroicons/react/outline'
-
 
 const ProductCard = ({pizza}) => {
-  const [counter, setCounter] = useState(0);
   return (
     <div className="bg-primary-light">
       <img src={pizza.photo} alt={pizza.name}/>
       <div className="px-2 pt-1 pb-4">
         <h2 className="font-bold">{pizza.name}</h2>
         <p className="overflow-hidden text-sm text-ellipsis h-11">{pizza.description}</p>
-        <p className="pt-2 mt-2 mb-3 text-center border-t border-secundary">Stock: {pizza.stock - counter}</p>
-        <div className="flex content-center justify-between w-1/2 mx-auto">
-          <ItemCount stock={pizza.stock}/>
-        </div>
-        <div className="mt-3 text-center">
-          <Button disabled={counter===0} onClick={()=> alert('Aca va la función agregar')}>
-            <ShoppingCartIcon className="relative w-5 mr-1" style={{top: "2px"}}/> <span>Agregar</span>
-          </Button>
-        </div>
+        <ItemCount stock={pizza.stock}/>
       </div>
     </div>
   )
